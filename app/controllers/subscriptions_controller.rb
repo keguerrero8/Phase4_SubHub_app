7c5ed5e399d055b082e03370f55d5ea205218405
Sarah Nosal <nosalsm@gmail.com>
2022-02-15 15:42:02 -0600

    subscriptions added to main page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,13 +3,13 @@ class SubscriptionsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
     def index
-        # user = User.find_by(id: session[:user_id])
-        # if user
-        #     render json: user.subscriptions
-        # else
-        #     render json: {errors: ["No user logged in"]}, status: 401
-        # end
-        render json: Subscription.all
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user.subscriptions
+        else
+            render json: {errors: ["No user logged in"]}, status: 401
+        end
+        # render json: Subscription.all
     end
 
     def update
--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,6 +1,7 @@
 import { Route } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
 import AddSub from './AddSub'
+import Subscription from './Subscription';
 
 function HomePage({user}) {
     const [subscriptions, setSubscriptions] = useState([])
@@ -10,15 +11,28 @@ function HomePage({user}) {
         .then(r => r.json())
         .then(res => {
             setSubscriptions(res)
-            console.log(subscriptions)
-
         })
     }, [])
+
 
     return (
         <div>
                 <Route exact path='/'>
                     <h2>Hello {user.username}</h2>
+                    {subscriptions.length > 0 ? (
+                        subscriptions.map((subscription) => (
+                            <Subscription key={subscription.id}
+                                name={subscription.name}
+                                link={subscription.image_url}
+                                date={subscription.payment_date}
+                                cost={subscription.monthly_price} 
+                            />
+                        ))
+                    ) : (
+                        <>
+                            <h2>You have no subscriptions</h2>
+                        </>
+                    )}
                     <h1 className='footer'>Total Monthly Spend: </h1>
                 </Route>
                 {/* <Route exact path='/login'><Login/></Route> */}
